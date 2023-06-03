664d4ef0b153220e493625a9ceb363d461d6a7b1
Jon Carlos O'Neil <jco103001@gmail.com>
2023-06-03 00:48:40 -0400

    - Moved some code around bc I forgot pyhton need some things to be ordered specifically -renamed some functions for clarity -fleshed out get_new_closing_daily_price to select closing price at specific row -started filling in pcode in while loop for backtesting -changed some method names in LL class for clarity -added length var to LL class to assist MA class -Created MA class, all methods are present for functionality still needs to be tested

--- main.py
+++ main.py
@@ -7,70 +7,45 @@ import asyncio
 import requests
 import pandas as pd
 from datetime import date, datetime
-#from ta.volatility import BollingerBands
-#from ta.momentum import RSIIndicator
+# from ta.volatility import BollingerBands
+# from ta.momentum import RSIIndicator
 from alpaca_trade_api.rest import REST, TimeFrame
 import json
 import backtrader as bt
 import backtrader.feeds as btfeeds
 
-
 apiKey = "PlaceHolder"
 secretKey = "PlaceHolder"
-
 
 # Imports for Paper Trading
 
 from alpaca.trading.client import TradingClient
-
 
 # ---- PAPER TRADING WITH ALGO ---- #
 
 # paper=True enables paper trading
 trading_client = TradingClient(apiKey, secretKey, paper=True)
 
-
-
-
-
-# Imports for Back Testing
-
+# Imports For Back Testing
 
 from alpaca.data import StockHistoricalDataClient, StockBarsRequest
 from alpaca.data import StockHistoricalDataClient
 
+# Functions Needed For Back Testing
 
-# ---- BACK TESTING --- #
-simulationStartDate = "2023-01-01"
-simulationEndDate = "2023-05-15"
-
-
-
-stock_client = StockHistoricalDataClient(apiKey,  secretKey)
-
-
-while 1 == 1:
-    # add new vals to moving average
-    # check if moving avergaes were crossed
-        #generate buy sell signal
-    ## verify possible singal
-        #execute based on signal
-    #break if no more data
-    break
-
-
-
-
-def get_day_by_day_price(symbol): # returns dataframe
+# Symbol hist function
+def get_symbol_history(symbol, simulationStartDate,
+                       simulationEndDate):  # returns dataframe, symbol string, simulationStartDate string, SimulationEndDate string
     request_params = StockBarsRequest(
-                        symbol_or_symbols=[symbol],
-                        timeframe=TimeFrame.Day,
-                        start=simulationStartDate,
-                        end= simulationEndDate,
-                    )
+        symbol_or_symbols=[symbol],
+        timeframe=TimeFrame.Day,
+        start=simulationStartDate,
+        end=simulationEndDate,
+    )
     symbol_bars = stock_client.get_stock_bars(request_params)
     return symbol_bars.df
-# Expected output format of get_day_by_day_price()
+
+# Expected output format of get_symbol_history(symbol)
 """
 symbol  		timestamp                		open  	    high	    low 	    close	    volume		    trade_count	    vwap
 BTC/USD 	    2022-09-01 05:00:00+00:00   	20049.0 	20285.0	    19555.0 	20160.0 	2396.3504   	18060.0		    19920.278135
@@ -82,6 +57,34 @@ BTC/USD 	    2022-09-01 05:00:00+00:00   	20049.0 	20285.0	    19555.0 	20160.0 
 		        2022-09-07 05:00:00+00:00   	18723.0 	19459.0 	18678.0 	19351.0 	2259.2351   	16204.0 		19123.487500
 """
 
+
+# get new price val function
+def get_new_closing_daily_price(day_iter, symbol_hist):  # day_iter int, symbol_bars df
+    return symbol_hist.loc[day_iter, 'close']
+
+
+# ---- BACK TESTING --- #
+simulationStartDate = "2023-01-01"
+simulationEndDate = "2023-05-15"
+
+generalElectricCoSymbol = "GE"
+
+stock_client = StockHistoricalDataClient(apiKey, secretKey)
+
+ge_hist = get_symbol_history(generalElectricCoSymbol, simulationStartDate, simulationEndDate)
+
+while 1 == 1:
+    day_iter = 0
+    # get new price val
+    newPrice = get_new_closing_daily_price(day_iter, ge_hist)
+    # add new vals to moving average
+    # check if moving avergaes were crossed
+    # generate buy sell signal
+    ## verify possible singal
+    # execute based on signal
+    # break if no more data
+    day_iter = + 1
+    break
 
 
 # ---- Linked List Class ---- #
@@ -95,9 +98,12 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
-    def append(self, data):
+    # append means add to tail
+    def newTail(self, data):
         new_node = Node(data)
+        self.length += 1
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -105,8 +111,10 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
 
-    def prepend(self, data):
+    # prepend means add to head
+    def newHead(self, data):
         new_node = Node(data)
+        self.length += 1
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -122,6 +130,8 @@ class LinkedList:
             self.tail = None
         else:
             self.head = self.head.next
+        self.length -= 1
+
 
     def delete_tail(self):
         if self.tail is None:
@@ -135,9 +145,56 @@ class LinkedList:
                 current = current.next
             current.next = None
             self.tail = current
+        self.length -= 1
+
 
     def print_list(self):
         current = self.head
         while current:
             print(current.data, end=" ")
             current = current.next
+
+
+class MovingAverage:
+    def __init__(self, time_period_days):
+        self.time_period_days = time_period_days
+        self.linked_list = self.__createLinkedList(self)
+        self.movingAvg = 0
+        self.movingAvgSum = 0
+
+
+    # ---- public methods ---- #
+
+    # get moving avg
+    def getMA(self):
+        if self.movingAvg is 0:
+            print("MA is 0 in getMA")
+            return 0
+        return self.movingAvg
+
+    # add new day data point
+    # needs to: get find new avg, delete oldest val if at limit, add newest val to front
+    def addNewDataPoint(self, newVal):
+        self.__calcNewMA(newVal)
+
+
+    # ---- private methods ---- #
+
+    # calc new avg
+    def __calcNewMA(self, newVal):
+        self.linked_list.newHead(newVal)
+        self.movingAvgSum =+ newVal
+        if self.linked_list.length == self.time_period_days:
+            self.movingAvgSum =- self.linked_list.tail
+            self.linked_list.delete_tail()
+
+        if self.movingAvg == 0:
+            self.movingAvg = newVal
+        else:
+            self.movingAvg = self.movingAvgSum / self.linked_list.length
+
+    # linked list creation
+    def __createLinkedList(self):
+        return LinkedList()
+
+    # ...
